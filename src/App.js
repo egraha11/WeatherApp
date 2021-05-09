bfd820d7831b0b35c454ae6ee4493268f5cbb888
egraha11 <47150176+egraha11@users.noreply.github.com>
2021-05-09 15:41:51 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import MainDisplay from './mainDisplay';
+
+function App() {
+  return (
+
+    <MainDisplay />
+  );
+
+}
+
+export default App;
+
+
+
+
+/**
+ *       //<header className="App-header">
+      //</header>
+ */
